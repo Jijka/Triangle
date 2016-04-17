607c1e96ff1fc3afb0a9afc1fdede88b728dd882
i.akhaltsev <i.akhaltsev@pflb.ru>
2016-04-17 17:34:17 +0300

    Initial Commit

---
+++ Triangle/Triangle.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using static System.Math;
+
+namespace Triangle
+{
+    public class Triangle
+    {
+        /// <summary> Полупериметр </summary>
+        private readonly double _p;
+
+        /// <summary> Создание нового треугольника </summary>
+        /// <exception cref="ArgumentException">Возникает при отрицательных длинах сторон треугольника 
+        /// и если треугольник не прямоугольный</exception>
+        /// <param name="a">Сторона треугольника</param>
+        /// <param name="b">Сторона треугольника</param>
+        /// <param name="c">Сторона треугольника</param>
+        public Triangle(double a, double b, double c)
+        {
+            IsValid(a, b, c);
+
+            A = a;
+            B = b;
+            C = c;
+
+            _p = (A + B + C)/2;
+        }
+
+        /// <summary> Сторона треугольника </summary>
+        public double A { get; }
+
+        /// <summary> Сторона треугольника </summary>
+        public double B { get; }
+
+        /// <summary> Сторона треугольника </summary>
+        public double C { get; }
+
+        /// <summary>
+        /// Проверка сторон треугольника на валидность
+        /// </summary>
+        /// <exception cref="ArgumentException">Возникает при отрицательных длинах сторон треугольника 
+        /// и если треугольник не прямоугольный</exception>
+        /// <param name="a">Сторона треугольника</param>
+        /// <param name="b">Сторона треугольника</param>
+        /// <param name="c">Сторона треугольника</param>
+        /// <param name="tolerance">Точность</param>
+        private static void IsValid(double a, double b, double c, double tolerance = 1E-15)
+        {
+            if ((a <= 0.0) || (b <= 0.0) || (c <= 0.0))
+                throw new ArgumentException("Неверные длины сторон");
+
+            var catetA = Min(a, c);
+            var catetB = Min(b, c);
+            var hypotenuse = Max(Max(a, b), c);
+
+            if (Abs(hypotenuse*hypotenuse - (catetA*catetA + catetB*catetB)) < tolerance) return;
+            throw new ArgumentException("Треугольник не прямоугольный");
+        }
+
+        /// <summary> Вычисляет площадь</summary>
+        /// <returns>Площадь треугольника</returns>
+        public double Area() => Sqrt(_p*(_p - A)*(_p - B)*(_p - C));
+
+        /// <summary> Вычисляет площадь</summary>
+        /// <returns>Площадь треугольника</returns>
+        public static double Area(double a, double b, double c)
+        {
+            IsValid(a, b, c);
+            var p = (a + b + c)/2;
+            return Sqrt(p*(p - a)*(p - b)*(p - c));
+        }
+    }
+}
---
+++ TriangleTests/TriangleTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TriangleTests
+{
+    [TestClass]
+    public class TriangleTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void NegativeSidesTest()
+        {
+            new Triangle.Triangle(-1.5, -2.1, -3.999);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ZeroSidesTest()
+        {
+            new Triangle.Triangle(0.0, 0.0, 0.0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void PiphagorTest()
+        {
+            new Triangle.Triangle(1, 9, 8);
+        }
+
+        [TestMethod]
+        public void AreaTest()
+        {
+            var triangle = new Triangle.Triangle(3, 4, 5);
+            Assert.AreEqual(6, triangle.Area());
+        }
+
+        [TestMethod]
+        public void StaticAreaTest()
+        {
+            Assert.AreEqual(6, Triangle.Triangle.Area(3, 4, 5));
+        }
+
+    }
+}
